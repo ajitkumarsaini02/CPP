c5d6ad5e753377533a6ddfde09069e466bab656d
ajitkumarsaini02 <ajitkumarsaini02@gmail.com>
2026-01-30 18:21:19 +0530

    ..

---
+++ 01_Basics/04_variable/01_program.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    // a b c d A B C D , _ , $ 
+    // dont't use  : !@#%^&*-';/\|
+    int a=10;
+    cout<<a<<endl;
+    int A=10;
+    cout<<A<<endl;
+    int _A=10;
+    cout<<_A<<endl;
+    int A1=10;
+    cout<<A1<<endl;
+    // int A,1=10;
+    // cout<<A,1<<endl;
+    // int A 1=10;
+    // cout<<A 1<<endl;
+    // int 1A=10;
+    // cout<<1A<<endl;
+    // int !A=10;
+    // cout<<!A<<endl;
+    // int @A=10;
+    // cout<<@A<<endl;
+    int x166=10;
+    cout<<x166<<endl;
+    int x1664=10;
+    cout<<x1664<<endl;
+    // int int=10;
+    // cout<<int<<endl;
+    // int float=10;
+    // cout<<float<<endl;
+    int float1=10;
+    cout<<float1<<endl;
+    cout<<"hii";
+}
