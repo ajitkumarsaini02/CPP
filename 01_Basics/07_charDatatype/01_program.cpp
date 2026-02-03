e37824f1e6938db651141f4ff2f9a5674347a39d
ajitkumarsaini02 <ajitkumarsaini02@gmail.com>
2026-02-03 22:10:38 +0530

    ..

---
+++ 01_Basics/07_charDatatype/01_program.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+   char ch='a';
+   cout<<ch<<endl;
+   ch='A';
+   cout<<ch<<endl;
+   ch='@';
+   cout<<ch<<endl;
+//    ch='ab';
+//    cout<<ch<<endl;
+   ch='a';
+   cout<<int(ch)<<endl;
+   ch='#';
+   cout<<int(ch);
+}
