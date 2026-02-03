50d893ed987bb9b8f6190dad8de9f89cb2cacd28
ajitkumarsaini02 <ajitkumarsaini02@gmail.com>
2026-02-03 22:09:10 +0530

    ..

---
+++ 01_Basics/05_input/02_program.cpp
@@ -0,0 +1,13 @@
+// Example : Calculating Area of a Circle
+
+#include<iostream>
+using namespace std;
+int main(){
+    float r;
+    cout<<"Enter Radius : ";
+    cin>>r;
+    float pi=3.1415;
+    float a;
+    a=pi*r*r;
+    cout<<"Area of Circle : "<<a<<endl;
+}
