9f8e9a02514a2ff49aec88e188b0b046218a5f6c
ajitkumarsaini02 <ajitkumarsaini02@gmail.com>
2026-02-06 22:57:14 +0530

    ..

---
+++ 03_Loops/01_whatAndWhyLoops/01_program.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    //10 times Good Morning
+    cout<<"Good Morning"<<endl;
+    cout<<"Good Morning"<<endl;
+    cout<<"Good Morning"<<endl;
+    cout<<"Good Morning"<<endl;
+    cout<<"Good Morning"<<endl;
+    cout<<"Good Morning"<<endl;
+    cout<<"Good Morning"<<endl;
+    cout<<"Good Morning"<<endl;
+    cout<<"Good Morning"<<endl;
+    cout<<"----------------------------"<<endl;
+
+    //automate , repition
+
+    for(int i=1;i<=10;i++){
+        cout<<"Good Morning"<<endl;
+    }
+}
