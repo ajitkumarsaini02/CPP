d874da3cf887ac1362f9c3eed8fdd162f4cf2083
ajitkumarsaini02 <ajitkumarsaini02@gmail.com>
2026-02-06 23:13:42 +0530

    ..

---
+++ 03_Loops/04_while loop & do while loop/03_program.cpp
@@ -0,0 +1,16 @@
+/*
+I
+:
+Print all alphabets (uppercase) with their ASCII
+values.
+*/
+
+#include<iostream>
+using namespace std;
+int main(){
+    int i=65;
+    while(i<=90){
+        cout<<(char)i<<endl;
+        i++;
+    }
+}
