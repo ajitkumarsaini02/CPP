7c9a67a0a32e7cfb0577c899366d6b2372335174
ajitkumarsaini02 <ajitkumarsaini02@gmail.com>
2026-02-15 07:58:00 +0530

    ..

---
+++ 04_PatternPrinting/03_Triangle Patterns/01_program.cpp
@@ -0,0 +1,23 @@
+/*
+Q5. Print the following pattern:
+*
+* *
+* * *
+* * * *
+*/
+
+
+
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter a row no. : ";
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+}
