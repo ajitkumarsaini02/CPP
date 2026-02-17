3e9a2cc9eb22405bd140683130980709669416c0
ajitkumarsaini02 <ajitkumarsaini02@gmail.com>
2026-02-17 21:13:10 +0530

    ..

---
+++ 04_PatternPrinting/04_Alphabet Patterns/02_program.cpp
@@ -0,0 +1,27 @@
+/*
+Q5. Print the following pattern:
+  *
+  *
+*****
+  *
+  *
+*/
+
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter a row no. : ";
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n;j++){
+            if(i==n/2+1 || j==n/2+1)
+            cout<<"* ";
+            else
+            cout<<"  ";
+        }
+        cout<<endl;
+    }
+
+    
+}
