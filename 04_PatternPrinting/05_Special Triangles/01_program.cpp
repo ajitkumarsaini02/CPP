98a766297216bbffc1364000f7a83a654ea7c7a6
ajitkumarsaini02 <ajitkumarsaini02@gmail.com>
2026-02-18 21:36:36 +0530

    ..

---
+++ 04_PatternPrinting/05_Special Triangles/01_program.cpp
@@ -0,0 +1,25 @@
+/*
+Q5. Print the following pattern:
+1
+2 3
+4 5 6
+7 8 9 10
+*/
+
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter a row no. : ";
+    cin>>n;
+    int a=1;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+           cout<<a<<" ";
+           a++;
+        }
+        cout<<endl;
+    }
+
+    
+}
