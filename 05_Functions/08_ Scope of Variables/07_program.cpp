b9a0cb1e0f35440b4f173315525293b7c4d223a5
ajitkumarsaini02 <ajitkumarsaini02@gmail.com>
2026-02-28 13:38:10 +0530

    ..

---
+++ 05_Functions/08_ Scope of Variables/07_program.cpp
@@ -0,0 +1,16 @@
+/*
+Bonus Ques : Print the factorials of first n numbers
+*/
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"Enter a number : ";
+    cin>>n;
+    int fact=1;
+    for(int i=1;i<=n;i++){
+        fact*=i;
+        cout<<"Factorial of "<<i<<" : "<<fact<<endl;
+    }
+}
