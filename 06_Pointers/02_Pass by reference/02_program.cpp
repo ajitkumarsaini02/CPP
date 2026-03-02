c3f9e41e220fcd3e845364b809a020888d6a3b03
ajitkumarsaini02 <ajitkumarsaini02@gmail.com>
2026-03-02 10:34:29 +0530

    ..

---
+++ 06_Pointers/02_Pass by reference/02_program.cpp
@@ -0,0 +1,27 @@
+
+#include<iostream>
+using namespace std;
+
+void swap(int &a,int &b){
+    int temp=a;
+    a=b;
+    b=temp;
+}
+
+int main(){
+    int x,y;
+
+    cout<<"Enter first number : ";
+    cin>>x;
+    cout<<"Enter second number : ";
+    cin>>y;
+
+
+    cout<<"Before swap ";
+    cout<<"x : "<<x<<" , "<<" y : "<<y<<endl;
+    
+    swap(x,y);
+    cout<<"After swap ";
+    cout<<"x : "<<x<<" , "<<" y : "<<y<<endl;
+    
+}
