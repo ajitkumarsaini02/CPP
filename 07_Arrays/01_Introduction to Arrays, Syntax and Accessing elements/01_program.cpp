edf193b3a52416d44c9307bc2ab9c07e2c640a8f
ajitkumarsaini02 <ajitkumarsaini02@gmail.com>
2026-03-09 07:16:32 +0530

    ..

---
+++ 07_Arrays/01_Introduction to Arrays, Syntax and Accessing elements/01_program.cpp
@@ -0,0 +1,15 @@
+
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[7];
+    arr[0]=5;
+    arr[1]=4;
+    arr[2]=6;
+    arr[3]=3;
+    arr[4]=6;
+    arr[5]=2;
+    arr[6]=8;
+
+    cout<<arr[0];
+}
