e69056a66d33c3f4728cf85657ca2666e9e6a904
ajitkumarsaini02 <ajitkumarsaini02@gmail.com>
2026-04-22 16:24:04 +0530

    ..

---
+++ 10_Strings/01_IntroductionToStrings/02_program.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    // string name="Raj kumar";
+    // cout<<name;
+    string name;
+    // cin>>name; //only if the given string is no spaces
+    getline(cin,name);
+    cout<<name;
+}
