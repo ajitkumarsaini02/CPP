c91108371bf0a67c1840c868fcd76351df5cb5e7
ajitkumarsaini02 <ajitkumarsaini02@gmail.com>
2026-04-27 18:18:28 +0530

    ..

---
+++ 10_Strings/02_BuiltInFunctions/01_program.cpp
@@ -0,0 +1,9 @@
+//find out length or size of string
+
+#include<iostream>
+using namespace std;
+int main(){
+    string str="knight means surveer";
+    cout<<"Size is "<<str.size()<<endl;
+    cout<<"Length is "<<str.length();
+}
