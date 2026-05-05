e11e17120ceee21448ce815c2f67eed4e625cad5
ajitkumarsaini02 <ajitkumarsaini02@gmail.com>
2026-05-06 00:11:32 +0530

    ..

---
+++ 10_Strings/02_BuiltInFunctions/09_program.cpp
@@ -0,0 +1,10 @@
+//to_string() --> change interger to string
+
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+    int n=12345;
+    string s=to_string(n);
+    cout<<s;
+}
